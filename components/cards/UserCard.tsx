9950abdcab4301d5b59d72c7436b25f4fb2000d3
mkostecki <kosteckimaciej@outlook.com>
2023-10-27 16:45:22 +0200

    feat/activity-page-final

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -16,13 +16,14 @@ const UserCard = ({ id, name, username, imgUrl, personType }: Props) => {
   return (
     <article className="user-card">
       <div className="user-card_avatar">
+      <div className='relative h-12 w-12'>
         <Image
           src={imgUrl}
           alt="logo"
-          width={48}
-          height={48}
+          fill
           className="rounded-full"
         />
+        </div>
         <div className="flex-1 text-ellipsis">
           <h4 className="text-base-semibold text-light-1">{name}</h4>
           <p className="text-small-medium text-gray-1">@{username}</p>
