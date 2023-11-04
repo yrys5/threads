5f3efa3766ac882204c81670b42255f6d44f9596
mkostecki <kosteckimaciej@outlook.com>
2023-11-04 11:40:27 +0100

    fix/googlebot-access-test-3

--- middleware.ts
+++ middleware.ts
@@ -1,36 +1,14 @@
-// import { authMiddleware } from "@clerk/nextjs";
-
-// // This example protects all routes including api/trpc routes
-// // Please edit this to allow other routes to be public as needed.
-// // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-// export default authMiddleware({
-//   debug: true,
-//   publicRoutes: ["/","/sign-in", "/sign-up" ,"/api/webhook/clerk"],
-//   ignoredRoutes: ["/api/webhook/clerk"],
-// });
-
-// export const config = {
-//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
-
 import { authMiddleware } from "@clerk/nextjs";
 
-const isGoogleBot = (userAgent) => {
-  // Regular expression to check for Googlebot's User-Agent string
-  return /Google-InspectionTool/.test(userAgent || '');
-};
-
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  beforeAuth: (req) => {
-    // Check if the User-Agent is Googlebot and bypass auth if true
-    if (isGoogleBot(req.headers["user-agent"])) {
-      return Promise.resolve(); // Bypass authentication
-    }
-  },
-
   debug: true,
-  publicRoutes: ["/", "/sign-in", "/sign-up", "/api/webhook/clerk"],
-  ignoredRoutes: ["/api/webhook/clerk"],
+  authorizedParties:['https://www.thrinks.com', 'http://localhost:3000'],
+  publicRoutes: ["/","/sign-in", "/sign-up" ,"/api/webhook/clerk"],
+  ignoredRoutes: ["/api/webhook/clerk", '/'],
+  
 });
 
 export const config = {
